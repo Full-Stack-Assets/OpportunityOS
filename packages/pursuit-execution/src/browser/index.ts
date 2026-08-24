651b45df76a3151edd246270aa2101e9f060bf3c
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-24 15:33:06 -0400

    feat(execution): export independent ATS verifier

--- packages/pursuit-execution/src/browser/index.ts
+++ packages/pursuit-execution/src/browser/index.ts
@@ -3,3 +3,4 @@ export * from './challenge-detector.ts';
 export * from './generic-driver.ts';
 export * from './provider-driver.ts';
 export * from './playwright-providers.ts';
+export * from './ats-verifier.ts';
