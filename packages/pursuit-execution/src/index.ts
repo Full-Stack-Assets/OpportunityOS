37ddd346c3667b16aac60959d737c19c371b8baf
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-24 12:50:29 -0400

    chore(execution): export trusted Freelancer bridge

--- packages/pursuit-execution/src/index.ts
+++ packages/pursuit-execution/src/index.ts
@@ -1,3 +1,5 @@
 export * from './encrypted-store.ts';
 export * from './brokers.ts';
 export * from './router.ts';
+export * from './freelancer-trusted-gateway.ts';
+export * from './python-gateway-transport.ts';
