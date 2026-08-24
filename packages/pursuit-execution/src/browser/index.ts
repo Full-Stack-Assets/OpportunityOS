5b227c924448ef3b9b5fdefef60d5bf86e803762
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-24 15:30:02 -0400

    feat(execution): export concrete ATS Playwright providers

--- packages/pursuit-execution/src/browser/index.ts
+++ packages/pursuit-execution/src/browser/index.ts
@@ -2,3 +2,4 @@ export * from './field-classifier.ts';
 export * from './challenge-detector.ts';
 export * from './generic-driver.ts';
 export * from './provider-driver.ts';
+export * from './playwright-providers.ts';
